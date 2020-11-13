4b2d4d65da3369bc774399fb3faf3ad64a373c40
Jackoberto <jakar17@elev.fryshuset.se>
2020-11-13 15:59:52 +0100

    Phone and popup cleanup

---
+++ Assets/Scripts/PhoneCallGenerator.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class PhoneCallGenerator : MonoBehaviour
+{
+    //private Animator phoneAnimator;
+    public int phoneCallRarity;
+    public float repeatTime = 20f;
+
+    //TODO Implement a exclamation mark spawner
+    void Start()
+    {
+        InvokeRepeating("IsCalling",2f, repeatTime);
+    }
+
+    private void IsCalling()
+    {
+        if (NumberGenerator() == phoneCallRarity - 1)
+        {
+               //Spawn Excalmation Mark 
+        }
+    }
+
+    private int NumberGenerator()
+    {
+        return Random.Range(0, phoneCallRarity);
+    }
+}
