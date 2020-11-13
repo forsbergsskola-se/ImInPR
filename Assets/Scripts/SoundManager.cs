59215e61a19f9941a394a03fd684a57b689ab787
Michael Van Der Poll <michaelvdp@gmail.com>
2020-11-13 13:13:57 +0100

    Imports GameSounds, PlayerBusinessCard art
    updates to soundClasses

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -12,31 +12,31 @@ public class SoundManager : MonoBehaviour
     [SerializeField] private GameSoundController musicController;
     [Range(0,1f)] public float musicVolume = 0.5f;
     [SerializeField] private GameObject nowPlayingPrefab;
-    [SerializeField] private Transform parent;
-    private GameSong currentSong;
+    //[SerializeField] private Transform parent;
+    private GameSong _currentSong;
     
-    /*public void PlaySound(string soundName)
-    {
-        var sound = gameSoundController.FindGameAudioClip(soundName);
-        Play(sound, gameSoundAudioSource, gameSoundVolume);
-    }*/
-
     public void PlaySong(string songName)
     {
         var sound = (GameSong)musicController.FindGameSound(songName);
-        Play(sound, musicAudioSource, musicVolume);
+        PlayMusic(sound, musicAudioSource, musicVolume);
     }
 
-    private void Play(GameSong sound, AudioSource source, float volume)
+    private void PlayMusic(GameSong sound, AudioSource source, float volume)
     {
         if (sound != null)
         {
             if(source.isPlaying)
                 source.Stop();
             
-            source.PlayOneShot(sound.clip, volume);
-            var instance = Instantiate(nowPlayingPrefab, parent);
+            Play(sound, source, volume);
+            var instance = Instantiate(nowPlayingPrefab, this.transform);
             instance.GetComponent<NowPlaying>().Setup(sound.bandName, sound.songName, sound.soundDesignerName);
         }
     }
+    private void Play(GameSound sound, AudioSource source, float volume)
+    {
+        source.PlayOneShot(sound.clip, volume);
+    }
+
+    
 }
