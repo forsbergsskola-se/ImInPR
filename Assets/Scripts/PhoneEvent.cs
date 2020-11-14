b3791b3c57667ee6329a04ced500edd8500b3f12
Jackoberto <jakar17@elev.fryshuset.se>
2020-11-14 14:26:01 +0100

    Adds PhoneEvents

---
+++ Assets/Scripts/PhoneEvent.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName = "Phone Event", fileName = "NewPhoneEvent")]
+public class PhoneEvent : ScriptableObject
+{
+    [TextArea] public string dialogue;
+    [TextArea] public string negativeOutcomeMessage;
+    [TextArea] public string positiveOutcomeMessage;
+    public int level;
+    [Range(0, 1)] [Tooltip("A Percentage Value Between 0 And 1")] 
+    public float successChance;
+}
