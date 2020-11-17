c64ec259d47fba1c4dee0ed3d90d0e0a36b6b8d7
Michael Van Der Poll <michaelvdp@gmail.com>
2020-11-17 11:57:45 +0100

    Updates to Player and BusinessCard

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -8,15 +9,16 @@ public class Player : MonoBehaviour
     [SerializeField] private int xpReqToLevel = 100;
     [SerializeField] private Image playerModel;
     [SerializeField] private Sprite[] models;
-
+    [SerializeField] private Image xpBar;
+    
     public event Action OnLevelUp;
+    //public event Action OnXPChanged;
     public int Level
     {
         get => PlayerPrefs.GetInt($"{this.name}_Level");
         private set
         {
             PlayerPrefs.SetInt($"{this.name}_Level", value);
-            
         }
     } 
 
@@ -31,14 +33,38 @@ public class Player : MonoBehaviour
     {
         if (value <= 0) return;
         
+        updateXpBar(playerXP.ExperienceAmount, playerXP.ExperienceAmount + value);
         playerXP.ExperienceAmount += value;
+        
         if (playerXP.ExperienceAmount >= xpReqToLevel)
         {
             //todo Notification that LevelUp is ready.
-            //perhaps Add levelUp to an onAcceptClicked Event on Notification?
-            
             
             LevelUp();
+        }
+    }
+
+    public void LoseXP(int value)
+    {
+        updateXpBar(playerXP.ExperienceAmount, playerXP.ExperienceAmount - value);
+        playerXP.ExperienceAmount -= value;
+        
+    }
+
+    public float xpPercentage()
+    {
+        return playerXP.ExperienceAmount / xpReqToLevel;
+    }
+
+    private void updateXpBar(float start, float end)
+    {
+        float elapsedTime = 0;
+        float timeToComplete = 1f;
+        
+        while (elapsedTime < timeToComplete)
+        {
+            elapsedTime += Time.deltaTime;
+            xpBar.fillAmount = Mathf.Lerp(start, end, elapsedTime);
         }
     }
 }
